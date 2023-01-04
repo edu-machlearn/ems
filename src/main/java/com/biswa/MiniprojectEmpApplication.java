c734cdb0b612c2a33e0c2dd1db3e29b4d6efc6e3
Biswaranjan Mohapatra <info.machlearn@gmail.com>
2023-01-04 21:51:16 +0530

    My First Commit After Doing the Project

---
+++ src/main/java/com/biswa/MiniprojectEmpApplication.java
@@ -0,0 +1,14 @@
+package com.biswa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniprojectEmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniprojectEmpApplication.class, args);
+		System.out.println("Employee Manage ment System...");
+	}
+
+}
